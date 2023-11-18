e6a08694f28f1df4a6cdb9b8e57f95894f2f7203
lirong <lirong@qq.com>
2023-11-18 16:17:15 +0800

    helper

---
+++ src/Helpers/CommonHelper.php
@@ -0,0 +1,36 @@
+<?php
+namespace Hexin\Library\Helpers;
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/10/22
+ * Time: 19:45
+ */
+class CommonHelper
+{
+    /**
+     * 是否Cli模式
+     * User: lir 2020/12/25 11:19
+     * @return array
+     */
+    public static function isCli()
+    {
+        if (preg_match("/cli/i", php_sapi_name())) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 是否生产环境
+     * User: lir 2020/9/2 16:02
+     * @return bool
+     */
+    public static function isProduction()
+    {
+        if(env('APP_ENV') != 'production'){
+            return false;
+        }
+        return true;
+    }
+}
