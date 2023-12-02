c4edfe907bfb2aaf20ad5288f7d2c413761d0b84
lirong <lirong@qq.com>
2023-12-02 10:17:26 +0800

    常量平台id

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -20,6 +20,46 @@ class Model extends LaravelModel
     protected $schema;
 
     /**
+     * 平台
+     */
+    const PLATFORM_AMAZON = 1;
+    const PLATFORM_ALIEXPRESS = 2;
+    const PLATFORM_WISH = 3;
+    const PLATFORM_EBAY = 4;
+    const PLATFORM_JOOM = 6;
+    const PLATFORM_WALMART = 8;
+    const PLATFORM_SHOPEE = 9;
+    const PLATFORM_SHOPIFY = 13;
+    const PLATFORM_VOVA = 14;
+    const PLATFORM_CDISCOUNT = 15;
+    const PLATFORM_LAZADA = 16;
+    const PLATFORM_ALIBABA = 17;
+    const PLATFORM_DARAZ = 18;
+    const PLATFORM_UD = 1000; //手工单暂定1000
+    const PLATFORM_HXCART = 22;
+    const PLATFORM_TIKTOK = 30;
+    const PLATFORM_QVC = 31;
+    public static $platformLabel = [
+        self::PLATFORM_AMAZON => 'Amazon',
+        self::PLATFORM_ALIEXPRESS => 'Aliexpress',
+        self::PLATFORM_WISH => 'Wish',
+        self::PLATFORM_EBAY => 'Ebay',
+        self::PLATFORM_JOOM => 'Joom',
+        self::PLATFORM_WALMART => 'Walmart',
+        self::PLATFORM_SHOPEE =>'Shopee',
+        self::PLATFORM_SHOPIFY =>'Shopify',
+        self::PLATFORM_VOVA => 'Vova',
+        self::PLATFORM_LAZADA => 'Lazada',
+        self::PLATFORM_ALIBABA => 'Alibaba',
+        self::PLATFORM_DARAZ => 'Daraz',
+        self::PLATFORM_CDISCOUNT => 'Cdiscount',
+        self::PLATFORM_HXCART => 'Hxcart',
+        self::PLATFORM_UD => 'Ud',
+        self::PLATFORM_TIKTOK => 'Tiktok',
+        self::PLATFORM_QVC => 'Qvc',
+    ];
+
+    /**
      * 增加商户id 默认值
      * @var array
      */
