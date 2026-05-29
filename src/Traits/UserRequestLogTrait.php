f39cdbea336c8dae73981c31c781e5093f40b262
李荣 <lirong@hexincorp.cn>
2026-05-29 11:35:16 +0800

    10.9用户权限

--- src/Traits/UserRequestLogTrait.php
+++ src/Traits/UserRequestLogTrait.php
@@ -28,16 +28,25 @@ public  function setBlackList(array $black_list):void
      * @param $route
      * @param $module
      */
-    public function setUserRequestLog($route,$module = '')
+    public function setUserRequestLog($route,$method,$module = '')
     {
         if(in_array($route,$this->blak_list)) {
         return ;
         }
+        $route_menu = [];
+        $route_menu_file =  file_get_contents(env('STAT_URL').'/route_menu.json');
+            if(!empty($route_menu_file)) {
+                $route_menu = json_decode($route_menu_file,true);
+            }
+        $menu_arr = $route_menu[$route.'-'.$method] ?? [];
+
         $data = [
             'route' => $route,
+            'method' => $method,
             'uuid' => config('userInfo')['uuid'] ?? '',
             'uname' => config('userInfo')['member_name'] ?? '',
-            'module' => $module ? $module : '',
+            'module' => $module ? $module : ($menu_arr ? $menu_arr['module'] : ''),
+            'action' => $menu_arr ? $menu_arr['action'] : $route,
             'create_time' => date('Y-m-d H:i:s'),
             'update_time' => date('Y-m-d H:i:s'),
         ];
